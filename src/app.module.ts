5a23b8dd388594d3c23ad9fbc570e33ee6636568
Anis Sheikh <anis.infowind@gmail.com>
2025-06-16 20:04:28 +0530

    Nest Middleware, exceptions filters, pipes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { SongsController } from './songs/songs.controller';
 
 @Module({
   imports: [SongsModule],
@@ -16,4 +18,13 @@ import { SongsModule } from './songs/songs.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    //consumer.apply(LoggerMiddleware).forRoutes('songs'); // option no 1
+    // consumer
+    // .apply(LoggerMiddleware)
+    // .forRoutes({ path: 'songs', method: RequestMethod.POST }); //option no 2
+
+    consumer.apply(LoggerMiddleware).forRoutes(SongsController); //option no 3
+  }
+}
