1a956403ec910fb7ce5a6158490fffbca6922658
Anis <anis.infowind@gmail.com>
2025-07-09 19:30:12 +0530

    Data seeding - add sample data during testing

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 // Force TypeORM to use mysql2 instead of broken mysql fallback
 import { PlatformTools } from 'typeorm/platform/PlatformTools';
+import { SeedService } from './seed/seed.service';
 
 PlatformTools.load = (name: string) => {
   if (name === 'mysql') {
@@ -14,6 +15,11 @@ PlatformTools.load = (name: string) => {
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  /**
+   * You can enable the seeding here
+   */
+  // const seedService = app.get(SeedService);
+  // await seedService.seed();
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
