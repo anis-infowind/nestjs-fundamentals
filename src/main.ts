45e0feb98545a5851da413b18e7392764a8653ad
Anis <anis.infowind@gmail.com>
2025-07-18 19:44:36 +0530

    Added Swagger module for API documentation

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 import { PlatformTools } from 'typeorm/platform/PlatformTools';
 import { SeedService } from './seed/seed.service';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 
@@ -26,6 +27,17 @@ async function bootstrap() {
    */
   // const seedService = app.get(SeedService);
   // await seedService.seed();
+
+  ///......
+  //Configure the swagger module here
+  const config = new DocumentBuilder() //1
+  .setTitle("Spotify Clone")
+  .setDescription("The Spotify Clone Api documentation")
+  .setVersion("1.0")
+  .build();
+  const document = SwaggerModule.createDocument(app, config); //2
+  SwaggerModule.setup("api-documentation", app, document); //3
+
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('port') ?? 3000);
 
