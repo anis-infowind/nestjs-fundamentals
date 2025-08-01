7c88e6156bee3c126a3899e58c1fe4cb86f13bae
Anis <anis.infowind@gmail.com>
2025-08-01 19:09:02 +0530

    File Upload feature added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, FileTypeValidator, Get, HttpStatus, MaxFileSizeValidator, ParseFilePipe, ParseFilePipeBuilder, Post, Req, UploadedFile, UseGuards, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { Request as ExpressRequest } from 'express';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller()
 @ApiTags('app')
@@ -27,5 +29,66 @@ export class AppController {
   @Get('test')
   testEnv() {
     return this.appService.getEnvVariables();
+  }
+
+  //File upload
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload/files',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  uploadFile(
+    @UploadedFile() file: Express.Multer.File
+  ) {
+    console.log(file);
+    return {
+      message: 'File uploaded successfully',
+      file,
+    };
+  }
+
+  // only want to accept png file
+  @Post('upload-png')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './upload/files',
+        filename: (req, file, cb) => {
+          cb(null, file.originalname);
+        },
+      }),
+    }),
+  )
+  uploadPngFile(
+    @UploadedFile(
+      // new ParseFilePipe({
+      //   validators: [
+      //     //new MaxFileSizeValidator({ maxSize: 1000 }),
+      //     new FileTypeValidator({ fileType: 'image/png' }),
+      //   ],
+      // }),
+      new ParseFilePipeBuilder()
+        .addFileTypeValidator({
+          fileType: 'image/png',
+        })
+        // .addMaxSizeValidator({
+        //   maxSize: 70706,
+        // })
+        .build({
+          errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+        }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file);
+    return {
+      message: 'file uploaded successfully!',
+    };
   }
 }
